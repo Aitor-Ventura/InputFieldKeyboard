fc479677cd58a52108157d207573ad5463ef4d05
Aitor Ventura <phsykox@gmail.com>
2023-09-21 16:09:03 +0100

    first iteration

--- Assets/MyInputField.cs
+++ Assets/MyInputField.cs
@@ -1,4 +1,71 @@
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
 
-public class MyInputField : InputFieldOriginal
+public class MyInputField : InputField
 {
+    protected override void Start()
+    {
+        base.Start();
+        onEndEdit.AddListener(DeactivateEdit);
+    }
+
+    public void AddCharacter(string character)
+    {
+        
+        string beforeText = text.Substring(0, caretPosition);
+        string afterText = text.Substring(caretPosition);
+        text = beforeText + character + afterText;
+
+        ForceCaretPosition(caretPosition + character.Length);
+        
+        Debug.Log("After adding a letter index: " + caretPosition);
+    }
+
+
+    public void DeleteCharacter()
+    {
+        Debug.Log("Before removing a letter index: " + caretPosition);
+        
+        if (caretPosition <= 0) return;
+
+        string beforeText = text.Substring(0, caretPosition - 1);
+        string afterText = text.Substring(caretPosition);
+        
+        ForceCaretPosition(caretPosition - 1);
+        
+        text = beforeText + afterText;
+
+        Debug.Log("After removing a letter index: " + caretPosition);
+    }
+
+    public void MoveCaretLeft()
+    {
+        if (caretPosition <= 0) return;
+        
+        ForceCaretPosition(caretPosition - 1);
+        Debug.Log("After moving caret left index: " + caretPosition);
+    }
+    
+    public void MoveCaretRight()
+    {
+        if (caretPosition >= text.Length) return;
+
+        ForceCaretPosition(caretPosition + 1);
+        Debug.Log("After moving caret right index: " + caretPosition);
+    }
+
+    private void ForceCaretPosition(int newPosition)
+    {
+        caretPosition = newPosition;
+        caretSelectPositionInternal = caretPosition;
+        
+        // Try forcing the update
+        UpdateLabel();
+    }
+
+    public void DeactivateEdit(string text)
+    {
+        DeactivateInputField();
+    }
 }
--- Assets/VirtualKeyboard.cs
+++ Assets/VirtualKeyboard.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,24 +8,82 @@ public class VirtualKeyboard : MonoBehaviour {
 
     public MyInputField InputField;
 
+    private enum KeyType
+    {
+        None,
+        Character,
+        Left,
+        Right,
+        Backspace
+    }
+
+    private KeyType heldKey = KeyType.None;
+    private string heldCharacter = "";
+    private float holdTimer = 0.0f;
+    private float keyRepeatDelay = 0.5f;
+    private float keyRepeatRate = 0.1f;
+
+    private void Update()
+    {
+        if (heldKey == KeyType.None) return;
+        
+        holdTimer -= Time.deltaTime;
+        if (holdTimer > 0.0f) return;
+
+        switch (heldKey)
+        {
+            case KeyType.Character:
+                InputField.AddCharacter(heldCharacter);
+                break;
+            case KeyType.Left:
+                InputField.MoveCaretLeft();
+                break;
+            case KeyType.Right:
+                InputField.MoveCaretRight();
+                break;
+            case KeyType.Backspace:
+                InputField.DeleteCharacter();
+                break;
+        }
+        
+        holdTimer = keyRepeatRate;
+    }
+
     public void KeyPress(string c)
     {
-        InputField.text += c;
-        InputField.ActivateInputField();
+        InputField.AddCharacter(c);
+        
+        heldKey = KeyType.Character;
+        heldCharacter = c;
+        holdTimer = keyRepeatDelay;
+    }
+
+    public void KeyRelease()
+    {
+        heldKey = KeyType.None;
     }
 
     public void KeyLeft()
     {
-
+        InputField.MoveCaretLeft();
+        
+        heldKey = KeyType.Left;
+        holdTimer = keyRepeatDelay;
     }
 
     public void KeyRight()
     {
-
+        InputField.MoveCaretRight();
+        
+        heldKey = KeyType.Right;
+        holdTimer = keyRepeatDelay;
     }
 
     public void KeyDelete()
     {
-
+        InputField.DeleteCharacter();
+        
+        heldKey = KeyType.Backspace;
+        holdTimer = keyRepeatDelay;
     }
 }
